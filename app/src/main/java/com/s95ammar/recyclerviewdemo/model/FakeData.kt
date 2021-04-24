833bf87b43f0234d080b8727e7623880857904f1
Ammar <ammar.courses@gmail.com>
2021-04-24 13:08:46 +0300

    Init

---
+++ app/src/main/java/com/s95ammar/recyclerviewdemo/model/FakeData.kt
@@ -0,0 +1,16 @@
+package com.s95ammar.recyclerviewdemo.model
+
+object FakeData {
+    val products = listOf(
+        Product(1, "Chips", 20, "MegaMarket"),
+        Product(2, "Ice cream", 10, "MegaMarket"),
+        Product(3, "Cheese", 60, "MegaMarket"),
+        Product(4, "Smartphone", 10000, "Rozetka"),
+        Product(5, "Laptop", 20000, "Rozetka"),
+        Product(6, "Headphones", 2000, "Eldoraro"),
+        Product(7, "Monitor", 6000, "Foxtrot"),
+        Product(8, "Scooter", 10000, "Foxtrot"),
+        Product(9, "Bicycle", 8000, "Foxtrot"),
+        Product(10, "Jeans", 1500, "Zara")
+    )
+}
---
+++ app/src/main/java/com/s95ammar/recyclerviewdemo/model/Product.kt
@@ -0,0 +1,8 @@
+package com.s95ammar.recyclerviewdemo.model
+
+data class Product(
+    val id: Int,
+    val name: String,
+    val price: Int,
+    val seller: String
+)
